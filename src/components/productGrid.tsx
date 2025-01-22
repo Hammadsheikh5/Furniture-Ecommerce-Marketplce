8ea3ee8693949b3bd01b7f9af96d14a3bc9df4cc
Hammad <mhammadkhi5@gmail.com>
2025-01-22 21:06:04 +0500

    Updated Code with GROQ Subscriptions

--- src/components/productGrid.tsx
+++ src/components/productGrid.tsx
@@ -1,22 +1,24 @@
-
+"use client";
 
 import { client } from "@/sanity/lib/client";
 import PaginationButtons from "./paginationButtons";
 import ProductCard from "./productCard";
 import Filter from "./filter";
+import { useState, useEffect } from "react";
 
 export interface Products {
   _id: string;
   name: string;
   image: string;
   price: number;
-  featuredProducts?: boolean;
+  isFeaturedProduct?: boolean;
   category?: string;
 }
 
-export default async function ProductGrid() {
-  const query = `*[_type == "product"] | order(_createdAt asc
-) {
+export default function ProductGrid() {
+  const [products, setProducts] = useState<Products[]>([]);
+
+  const query = `*[_type == "product"] | order(_createdAt asc) {
     name,
     _id,
     "image": image.asset->url,
@@ -25,20 +27,24 @@ export default async function ProductGrid() {
     category
   }`;
 
-  let products: Products[] = [];
+  useEffect(() => {
+    // Initial fetch
+    client.fetch(query).then((data) => setProducts(data));
 
-  try {
-    products = await client.fetch(query);
-  } catch (error) {
-    console.error("Error fetching products:", error);
-  }
+    // Setup GROQ Subscription for real-time updates
+    const subscription = client.listen(query).subscribe((update) => {
+      client.fetch(query).then((data) => setProducts(data));
+    });
 
+    // Cleanup the subscription on unmount
+    return () => subscription.unsubscribe();
+  }, []);
 
   return (
     <div className="font-poppins text-black bg-white">
-      <Filter/>
+      <Filter />
       <div className="w-full max-w-[1536px] mx-auto flex flex-col items-center px-[50px] lg:px-[100px]">
-        <div className="container pt-[50px] md:pt-[88px] flex flex-col md:flex-row md:grid md:grid-cols-2 xl:grid-cols-4 gap-4 md:gap-8 ">
+        <div className="container pt-[50px] md:pt-[88px] flex flex-col md:flex-row md:grid md:grid-cols-2 xl:grid-cols-4 gap-4 md:gap-8">
           {products.map((product) => (
             <ProductCard
               key={product._id}
